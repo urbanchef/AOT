3c84731420fba44c62683187ede5942de62b6279
Roman Musk <roman.mushchinskiy@gmail.com>
2018-03-27 16:04:28 +0300

    script to unpack all .war, .ear and .jar files in the project

--- unpack_all.py
+++ unpack_all.py
@@ -2,7 +2,6 @@ import sys
 import os
 import zipfile
 import logging
-import subprocess
 
 
 def unzip_source_zip(source_zip_path, unzip_dir):
@@ -17,6 +16,19 @@ def unzip_source_zip(source_zip_path, unzip_dir):
     return os.path.join(unzip_dir, root_dir)
 
 
+def _extract_file(absolute_file_name, extract_dir):
+    os.makedirs(extract_dir, exist_ok=True)
+    logging.debug('Extracting {} to {}'.format(absolute_file_name, extract_dir))
+    try:
+        with zipfile.ZipFile(absolute_file_name, 'r') as zip_ref:
+            zip_ref.extractall(extract_dir)
+    except zipfile.BadZipfile:
+        logging.error('File {} is corrupted'.format(absolute_file_name))
+    finally:
+        logging.info('Skipping ...')
+        pass
+
+
 def unzip_source_dir(target_dir):
     os.chdir(target_dir)
     logging.info('Extracting all .war, .ear and .jar under {}'.format(target_dir))
@@ -24,23 +36,18 @@ def unzip_source_dir(target_dir):
         for f in files:
             absolute_file_name = os.path.join(root, f)
             if f.endswith('.war'):
-                extract_dir = os.path.join(root, os.path.splitext(f)[0]) + '_war'
-                os.makedirs(extract_dir, exist_ok=True)
+                extract_dir = os.path.join(root, os.path.splitext(f)[0]) + '-war'
                 logging.debug('Found .war file: {}'.format(absolute_file_name))
-                logging.debug('Extracting {} to {}'.format(absolute_file_name, extract_dir))
-                subprocess.check_call(['unzip', '-d', extract_dir, absolute_file_name])
+                _extract_file(absolute_file_name, extract_dir)
             elif f.endswith('.ear'):
-                extract_dir = os.path.join(root, os.path.splitext(f)[0]) + '_ear'
-                os.makedirs(extract_dir, exist_ok=True)
+                extract_dir = os.path.join(root, os.path.splitext(f)[0]) + '-ear'
                 logging.debug('Found .ear file: {}'.format(absolute_file_name))
-                logging.debug('Extracting {} to {}'.format(absolute_file_name, extract_dir))
-                subprocess.check_call(['jar', '-xf', absolute_file_name, '-C', extract_dir])
+                _extract_file(absolute_file_name, extract_dir)
             elif f.endswith('.jar'):
-                extract_dir = os.path.join(root, os.path.splitext(f)[0]) + '_jar'
-                os.makedirs(extract_dir, exist_ok=True)
+                extract_dir = os.path.join(root, os.path.splitext(f)[0]) + '-jar'
                 logging.debug('Found .jar file: {}'.format(absolute_file_name))
-                logging.debug('Extracting {} to {}'.format(absolute_file_name, extract_dir))
-                subprocess.check_call(['jar', '-xf', absolute_file_name, '-C', extract_dir])
+                _extract_file(absolute_file_name, extract_dir)
+
 
 
 if __name__ == '__main__':
