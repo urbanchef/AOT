435c0aefbb5926c374b8112dd71c81742c9db18a
Roman Musk <roman.mushchinskiy@gmail.com>
2018-03-28 14:30:02 +0300

    open files with encoding

--- find_source.py
+++ find_source.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import logging
 
 
 def find_files(search_dir, str_pattern):
@@ -9,22 +8,17 @@ def find_files(search_dir, str_pattern):
     for root, dirs, files in os.walk(search_dir, topdown=True):
         for file in files:
             if file.endswith('.java'):
-                for line in file:
-                    if str_pattern in line:
-                        file_list.append(file)
+                absolute_path = os.path.join(root, file)
+                with open(absolute_path, 'r', encoding='latin-1') as file_handle:
+                    for line in file_handle:
+                        if str_pattern in line:
+                            file_list.append(absolute_path)
+                            break
     return file_list
 
 
-
 if __name__ == '__main__':
-    logger = logging.getLogger('find_constants_usage')
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
 
     search_dir = sys.argv[1]
     str_pattern = sys.argv[2]
-    find_files(search_dir, str_pattern)
+    l = [print(f) for f in find_files(search_dir, str_pattern)]
